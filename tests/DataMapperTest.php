7fd38f155cbdc254a518cfae09b0af4fe026a924
yangyi <yangyi.cn.gz@gmail.com>
2013-01-18 11:32:14 +0800

    增加DataMapper\Data 属性读写测试用例

--- tests/DataMapperTest.php
+++ tests/DataMapperTest.php
@@ -45,4 +45,117 @@ class DataMapperData extends \PHPUnit_Framework_TestCase {
 
         $this->assertEquals($d->p, 0);
     }
+
+    public function testSetProp() {
+        $this->setPropsMeta(array(
+            'id' => array('type' => 'integer', 'primary_key' => true, 'auto_increase' => true),
+            'email' => array('type' => 'string', 'refuse_update' => true),
+            'name' => array('type' => 'string', 'allow_null' => true),
+        ));
+
+        $class = $this->class;
+        $data = new $class;
+        $this->assertTrue($data->isFresh());
+
+        $data->email = '';
+        $this->assertNull($data->email, '对string类型的属性赋值\'\'应该转换为null');
+
+        $data->email = 'yangyi.cn.gz@gmail.com';
+        $data->name = 'yangyi';
+        $this->assertTrue($data->isDirty());
+
+        $data->save();
+        $this->assertFalse($data->isFresh());
+        $this->assertFalse($data->isDirty());
+
+        // 对属性设置同样的值，应该不会把属性标记为已修改
+        $data->name = 'yangyi';
+        $this->assertFalse($data->isDirty());
+        $data->destroy();
+
+        $data = new $class;
+        try {
+            $data->save();
+            $this->fail('属性的not allow null没有生效');
+        } catch (\Lysine\DataMapper\NullNotAllowedError $ex) {
+        }
+
+        //////////////////// refuse update ////////////////////
+        $props = array(
+            'id' => 1,
+            'email' => 'yangyi.cn.gz@gmail.com',
+            'name' => 'yangyi',
+        );
+        $data = new $class($props, $is_fresh = false);
+
+        $test = false;
+        try {
+            $data->email = 'yangyi.cn.gz@gmail.com';
+        } catch (\Lysine\DataMapper\RefuseUpdateError $ex) {
+            $test = true;
+        }
+        $this->assertTrue($test, '属性的refuse update没有生效');
+
+        $test = false;
+        try {
+            $data->id = 2;
+        } catch (\Lysine\DataMapper\RefuseUpdateError $ex) {
+            $test = true;
+        }
+        $this->assertTrue($test, '主键应该refuse update');
+
+        //////////////////// null not allowed ////////////////////
+        $data = new $class();
+
+        $test = false;
+        try {
+            $data->save();
+        } catch (\Lysine\DataMapper\NullNotAllowedError $ex) {
+            $test = true;
+        }
+        $this->assertTrue($test);
+
+        $test = false;
+        $data->email = 'yangyi.cn.gz@gmail.com';
+        try {
+            $data->save();
+        } catch (\Lysine\DataMapper\NullNotAllowedError $ex) {
+            $test = true;
+        }
+        $this->assertFalse($test);
+
+        $this->setPropsMeta(array(
+            'id' => array('type' => 'integer', 'primary_key' => true),
+        ));
+
+        $class = $this->class;
+        $data = new $class();
+
+        $test = false;
+        try {
+            $data->save();
+        } catch (\Lysine\DataMapper\NullNotAllowedError $ex) {
+            $test = true;
+        }
+        $this->assertTrue($test);
+    }
+
+    public function testGetProp() {
+        $this->setPropsMeta(array(
+            'id' => array('type' => 'integer', 'primary_key' => true, 'auto_increase' => true),
+            'email' => array('type' => 'string', 'refuse_update' => true),
+            'name' => array('type' => 'string', 'allow_null' => true),
+        ));
+
+        $class = $this->class;
+        $data = new $class;
+
+        $test = false;
+        try {
+            $data->passwd;
+        } catch (\Lysine\DataMapper\UndefinedPropertyError $ex) {
+            $test = true;
+        }
+        $this->assertTrue($test);
+    }
 }
