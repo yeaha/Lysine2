c0bea7e04b539dc996941b3e6656cf4da5047007
yangyi <yangyi.cn.gz@gmail.com>
2013-01-18 18:20:03 +0800

    增加Service\DB\Select测试用例

--- tests/Fixture/config/services.php
+++ tests/Fixture/config/services.php
@@ -5,6 +5,15 @@ return array(
         'host' => '127.0.0.1',
         'database' => 0,
     ),
+    'pgsql.local' => array(
+        'class' => '\Lysine\Service\DB\Adapter\Pgsql',
+        'user' => 'dev',
+        'password' => 'abc',
+        'dsn' => 'pgsql:host=127.0.0.1 port=5432 dbname=lysine',
+        'options' => array(
+            \PDO::ATTR_TIMEOUT => 3,
+        ),
+    ),
     'mock.storage' => array(
         'class' => '\Test\Mock\DataMapper\Storage',
     ),
