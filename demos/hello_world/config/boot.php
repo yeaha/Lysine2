9f3967ac76c98ba9cdf9d4a6f8ce44c9144e3f14
yangyi <yangyi.cn.gz@gmail.com>
2013-01-23 15:42:18 +0800

    增加注释

--- demos/hello_world/config/boot.php
+++ demos/hello_world/config/boot.php
@@ -8,19 +8,23 @@ define('DEBUG', true);
 require __DIR__ .'/../../../src/loader.php';
 require ROOT_DIR .'/lib/mvc.php';
 
+// 加载配置文件
 $config = require __DIR__ .'/_config.php';
-
 Config::import($config);
 
+// 初始化外部存储服务管理
 Lysine\Service\Manager::getInstance()
     ->importConfig(Config::get('services'));
 
+// 系统日志
 Lysine\logger()
     ->setLevel(DEBUG ? Lysine\Logging::DEBUG : Lysine\Logging::ERROR)
     ->addHandler(new Lysine\Logging\FileHandler(Config::get('logging')));
 
+// 使用内置SESSION封装
 Lysine\Session::initialize();
 
+// MVC环境初始化
 $app = new MVC\Application(Config::get('application'));
 $app->setRouter( new MVC\Router(Config::get('router')) );
 
