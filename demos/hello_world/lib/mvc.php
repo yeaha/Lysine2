db04e3905c400bacd1c4c12ac5e619e7450fc78b
yangyi <yangyi.cn.gz@gmail.com>
2013-03-30 15:05:42 +0800

    代码改进

--- demos/hello_world/lib/mvc.php
+++ demos/hello_world/lib/mvc.php
@@ -1,10 +1,10 @@
 <?php
 class Controller {
     static public function view() {
-        return new Lysine\MVC\View( ROOT_DIR .'/view' );
+        return new \Lysine\MVC\View( ROOT_DIR .'/view' );
     }
 
     protected function render($view, array $vars = array()) {
-        return self::view()->render($view, $vars);
+        return static::view()->render($view, $vars);
     }
 }
