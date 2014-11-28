cc2716fa7b445a1140fb8a0f47199035e4ba2fac
yangyi <yangyi.cn.gz@gmail.com>
2014-11-28 11:19:44 +0800

    添加注释

--- src/service/memcached.php
+++ src/service/memcached.php
@@ -1,14 +1,31 @@
 <?php
 namespace Lysine\Service;
 
-if (!extension_loaded('memcached'))
+if (!extension_loaded('memcached')) {
     throw new \RuntimeException('Require memcached extension');
+}
 
 class Memcached implements \Lysine\Service\IService {
     protected $handler;
     protected $config;
 
-    public function __construct(array $config=array()) {
+    /**
+     * @param array $config
+     *
+     * @example
+     * new \Lysine\Service\Memcached(array(
+     *     'servers' => array(
+     *         array('192.168.1.2', 11211, 10),
+     *         array('192.168.1.3', 11211, 20),
+     *         // ...
+     *     ),
+     *     'options' => array(  // 可选
+     *         \Memcached::OPT_PREFIX_KEY => "foobar",
+     *         // ...
+     *     ),
+     * ));
+     */
+    public function __construct(array $config = array()) {
         $this->config = $config;
     }
 
@@ -22,29 +39,43 @@ class Memcached implements \Lysine\Service\IService {
              : $this->connect()->$fn();
     }
 
+    /**
+     * 连接memcached
+     *
+     * @return \Memcached
+     */
     public function connect() {
-        if ($this->handler)
+        if ($this->handler) {
             return $this->handler;
+        }
 
         $servers = isset($this->config['servers'])
                  ? $this->config['servers']
                  : array(array('127.0.0.1', 11211));
 
         $handler = new \Memcached;
-        if (!$handler->addServers($servers))
+        if (!$handler->addServers($servers)) {
             throw new \Lysine\Service\ConnectionException('Cannot connect memcached');
+        }
 
         if (isset($config['options'])) {
-            foreach ($config['options'] as $key => $val)
-                $handler->setOption($key, $val);
+            $handler->setOptions($config['options']);
         }
 
         return $this->handler = $handler;
     }
 
+    /**
+     * 断开连接
+     *
+     * @return $this
+     */
     public function disconnect() {
-        if ($this->handler)
+        if ($this->handler) {
+            $this->handler->quit();
             $this->handler = null;
+        }
+
         return $this;
     }
 }
