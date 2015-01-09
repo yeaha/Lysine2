94951fb5828e883d8aa499875e0abb3f25c65131
yangyi <yangyi.cn.gz@gmail.com>
2015-01-09 16:41:17 +0800

    mysql adapter，默认禁用buffered query特性

--- src/service/db/mysql.php
+++ src/service/db/mysql.php
@@ -12,4 +12,15 @@ class Mysql extends \Lysine\Service\DB\Adapter {
     public function lastId($table = null, $column = null) {
         return $this->execute('SELECT last_insert_id()')->getCol();
     }
+
+    static protected function prepareConfig(array $config) {
+        list($dsn, $user, $password, $options) = parent::prepareConfig($config);
+
+        // 默认禁用buffered query特性
+        if (!isset($options[\PDO::MYSQL_ATTR_USE_BUFFERED_QUERY])) {
+            $options[\PDO::MYSQL_ATTR_USE_BUFFERED_QUERY] = false;
+        }
+
+        return array($dsn, $user, $password, $options);
+    }
 }
