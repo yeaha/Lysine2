a45576d2d94e1c01a574610dc7dc5918707adc09
yangyi <yangyi.cn.gz@gmail.com>
2016-01-18 22:34:17 +0800

    调整单元测试脚本

--- tests/BootStrap.php
+++ tests/BootStrap.php
@@ -1,24 +1,7 @@
 <?php
-namespace Test;
-
-define('TEST_DIR', __DIR__);
 define('LYSINE_REQUEST_CLASS', '\Test\Mock\Request');
 define('LYSINE_RESPONSE_CLASS', '\Test\Mock\Response');
 
-require __DIR__ .'/../src/loader.php';
-
-spl_autoload_register(function($class) {
-    $parts = array_map('ucfirst', explode('\\', trim($class, '\\')));
-    if ($parts[0] != 'Test') return false;
-
-    array_shift($parts);
-
-    $file = __DIR__ .'/'. implode('/', $parts) .'.php';
-    if (!is_file($file)) return false;
-
-    require $file;
-
-    return class_exists($class, false) || interface_exists($class, false);
-});
+require __DIR__ .'/../vendor/autoload.php';
 
 \Lysine\Service\Manager::getInstance()->importConfig(require __DIR__.'/Fixture/config/services.php');
